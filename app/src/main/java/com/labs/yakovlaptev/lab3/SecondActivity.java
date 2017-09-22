a54f51cf4a86aa98afab80f3754c8b9d6313ec75
Yakov <yakovlaptev@yandex.ru>
2017-09-22 22:32:26 +0300

    Example done.

---
+++ app/src/main/java/com/labs/yakovlaptev/lab3/SecondActivity.java
@@ -0,0 +1,53 @@
+package com.labs.yakovlaptev.lab3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends Activity {
+
+    final String TAG = "States";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+        Log.d(TAG, "SecondActivity: onCreate()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "SecondActivity: onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "SecondActivity: onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "SecondActivity: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "SecondActivity: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "SecondActivity: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "SecondActivity: onDestroy()");
+    }
+}
